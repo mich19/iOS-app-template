27d177b0a4ed736de99fdb6fff4034f57cf2b667
Michel Franco <michel.franco@payclip.com>
2020-05-30 17:29:42 -0500

    Initial Commit

---
+++ iOS-app-template/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-app-template
+//
+//  Created by Michel Franco Téllez on 30/05/20.
+//  Copyright © 2020 MichelFranco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
